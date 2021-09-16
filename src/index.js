3aa9adf1490f00d56212ebdf28a316f12a2c7e54
giuseppesperanza1996 <giuseppesperanza1996@gmail.com>
2021-09-16 14:27:05 +0200

    feature: added css loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './style.css'; // --> import del file css
+
 function drawComponent() {
   // crea un elemento HTML
   const element = document.createElement('div');
@@ -5,10 +7,12 @@ function drawComponent() {
   // Aggiunge la scritta Hello World
   element.innerHTML = "Hello World";
 
+  // Aggiunge la classe CSS create nel file style.css
+  element.classList.add('red');
+
   return element;
 }
 
 // "Appende" al body il ritorno della funzione drawComponent()
 document.body.appendChild(drawComponent());
-
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,17 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js', // file iniziale letto da webpack
   output: {
-    filename: 'main.js', // file generato per il bundle
+    filename: 'bundle.js', // file generato per il bundle
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // -> Regex per carcare solo file CSS
+        use: ['style-loader', 'css-loader'] // -> Loader da usare e in che ordine
+      }
+    ]
+  }
 };
+
 
